49fe1e7d8642cbd9f4e1b825f61765c18653aeda
Leandro de Oliveira <oliveira.dev1997@gmail.com>
2020-07-16 20:58:33 -0300

    Initial Commit

---
+++ App Store/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  App Store
+//
+//  Created by Leandro de Oliveira Novais on 16/07/20.
+//  Copyright © 2020 SpartanLab. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
